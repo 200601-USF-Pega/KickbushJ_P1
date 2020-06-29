e235753602dc3b816de8a4ef498b81cd607030da
jamakick <jamakick@iu.edu>
2020-06-28 20:54:24 -0400

    Commit before restructuring project for logging

--- src/com/jamakick/santasWorkshop2/web/ConnectionManager.java
+++ src/com/jamakick/santasWorkshop2/web/ConnectionManager.java
@@ -3,13 +3,8 @@ package com.jamakick.santasWorkshop2.web;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 
 public class ConnectionManager {
-	
-	private static final Logger logger = LogManager.getLogger(ConnectionManager.class);
 
 	private static Connection connection;
 	
--- src/com/jamakick/santasWorkshop2/web/LoginService.java
+++ src/com/jamakick/santasWorkshop2/web/LoginService.java
@@ -7,6 +7,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.jamakick.santasWorkshop2.dao.LoginDAO;
 import com.jamakick.santasWorkshop2.db.LoginImp;
 import com.jamakick.santasWorkshop2.object.LoginInfo;
@@ -16,6 +19,8 @@ import com.jamakick.santasWorkshop2.object.LoginReturn;
 public class LoginService {
 	
 	LoginDAO loginImp = new LoginImp();
+	
+	private static final Logger logger = LogManager.getLogger(LoginService.class);
 	
 	@POST
 	@Path("login/{id},{pass}")
@@ -27,11 +32,14 @@ public class LoginService {
 		
 		if (info.getPass().equals(pass)) {
 			output.setCorrectLoginInfo(true);
+			logger.debug("Login Succeed");
 		}
 		
 		else {
 			output.setCorrectLoginInfo(false);
+			logger.debug("Login Fail");
 		}
+	
 		
 		return Response.ok(output).build();
 		
